e9ca0395850ba2709e9dc3f390cea8bffe50f8a4
atarbabgei <atarbabgei@gmail.com>
2024-12-26 20:27:52 +0000

    added propeller_guard_joint

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -7,19 +7,26 @@ def generate_launch_description():
     return LaunchDescription([
         # Execute the PX4 SITL build and run
         ExecuteProcess(
-            cmd=['bash', '-c', 'cd ~/Firmware-PX4/rolling_drone/PX4-Autopilot && make px4_sitl gz_x500_spinning_guard'],
+            cmd=['bash', '-c', 'cd ~/Firmware-PX4/passive-rotating-prop/PX4-Autopilot && make px4_sitl gz_x500_passive_rotating_prop'],
             output='screen'
         ),
-
         # Launch the micro_ros_agent
         ExecuteProcess(
             cmd=['ros2', 'run', 'micro_ros_agent', 'micro_ros_agent', 'udp4', '-p', '8888'],
             output='screen'
         ),
         
-        # Launch the ros_gz_bridge for parameter_bridge
+        # Launch the ros_gz_bridge for force/torque parameter bridge
         ExecuteProcess(
-            cmd=['ros2', 'run', 'ros_gz_bridge', 'parameter_bridge', '/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model'],
+            cmd=['ros2', 'run', 'ros_gz_bridge', 'parameter_bridge', 
+                 '/propeller_guard_joint/force_torque@geometry_msgs/msg/Wrench@gz.msgs.Wrench'],
+            output='screen'
+        ),
+        
+        # Launch the ros_gz_bridge for joint state parameter bridge
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'ros_gz_bridge', 'parameter_bridge',
+                 '/propeller_guard_joint/joint_state@sensor_msgs/msg/JointState@gz.msgs.Model'],
             output='screen'
         ),
         
